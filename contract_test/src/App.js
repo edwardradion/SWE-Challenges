408aaa36be652f97f80a392e65fe46f176db460e
Edward Radion <er2cg@virginia.edu>
2018-06-04 17:01:13 -0400

    new updates, including functionality with entering textboxes.

--- contract_test/src/App.js
+++ contract_test/src/App.js
@@ -2,17 +2,44 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Form from "./Form.js";
+//import firebase from "https://www.gstatic.com/firebasejs/5.0.4/firebase.js";
+
+// Initialize Firebase
+// var config = {
+//   apiKey: "AIzaSyDnUgzcxDZqvXTJ1b_OE49ybacRw7F6lJM",
+//   authDomain: "contracttest-70b40.firebaseapp.com",
+//   databaseURL: "https://contracttest-70b40.firebaseio.com",
+//   projectId: "contracttest-70b40",
+//   storageBucket: "contracttest-70b40.appspot.com",
+//   messagingSenderId: "737784620167"
+// };
+// firebase.initializeApp(config);
 
 class App extends Component {
+  state = {
+    fields: {}
+  };
+
+  // messagesRef = firebase.database().ref('messages');
+
+  // document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+  onSubmit = fields => {
+    this.setState({ fields });
+    //fields.push(fields);
+  };
+
   render() {
     return (
       <div className="App">
-        <div>
-          <Form />
-        </div>
+        Enter your contract information below
+        <Form onSubmit={fields => this.onSubmit(fields)} />
+        <div>{JSON.stringify(this.state.fields, null, 2)}</div>
       </div>
     );
   }
 }
 
 export default App;
+
+//I'm not sure how to upload forms without using getElementById
--- contract_test/src/Form.js
+++ contract_test/src/Form.js
@@ -1,30 +1,78 @@
 import React from "react";
 
-export default class contractform extends React.Component {
+export default class Form extends React.Component {
   state = {
-    name: "",
+    yourname: "",
     company: "",
     details: ""
   };
+
+  change = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    // name = getInputVal("name");
+    // company = getInputVal("company");
+    // details = getInputVal("details");
+
+    // saveMessage(name, company, details);
+
+    //VIDEO ENTRY
+    this.props.onSubmit(this.state);
+    this.setState({
+      yourname: "",
+      company: "",
+      details: ""
+    });
+  };
+
+  //get form value
+  getInputVal(id) {
+    return document.getElementById(id).value;
+  }
+
+  //save to Firebase
+  // saveMessage(name, company, details) {
+  //   newMessageRef = messageRef.push();
+  //   newMessageRef.set({
+  //     name: name,
+  //     company: company,
+  //     details: details
+  //   });
+  // }
 
   render() {
     return (
       <form>
         <input
+          name="yourname"
+          id="yourname"
           placeholder="Name"
-          value={this.state.name}
-          onChange={e => this.setState({ name: e.target.value })}
+          value={this.state.yourname}
+          onChange={e => this.change(e)}
         />
+        <br />
         <input
+          name="company"
+          id="company"
           placeholder="Company"
           value={this.state.company}
-          onChange={e => this.setState({ company: e.target.value })}
+          onChange={e => this.change(e)}
         />
+        <br />
         <input
+          name="details"
+          id="details"
           placeholder="Contract Details"
           value={this.state.details}
-          onChange={e => this.setState({ details: e.target.value })}
+          onChange={e => this.change(e)}
         />
+        <br />
+        <button onClick={e => this.onSubmit(e)}>Submit</button>
       </form>
     );
   }
